c08fabe77ff38e5c75c36621c61385b24ad0ca18
Mohammed Hamouda <51118790+MoHamouda97@users.noreply.github.com>
2019-06-09 17:09:46 +0300

    Update register.component.ts

--- parashot/src/app/modules/auth/register/register.component.ts
+++ parashot/src/app/modules/auth/register/register.component.ts
@@ -1,3 +1,6 @@
+import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import {Headers, RequestOptions} from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 import { FacebookLoginProvider, GoogleLoginProvider, AuthService } from "angularx-social-login";
 
@@ -6,9 +9,42 @@ import { FacebookLoginProvider, GoogleLoginProvider, AuthService } from "angular
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+export class RegisterComponent implements OnInit { 
+
+  // private variables
+  
+  private apiURL = 'http://parashote.codesroots.com/api/users/add.json';
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
+  
+  addNewUser() { 
+    
+    // get user data
+        
+    let userData = {
+        
+        username: document.getElementById('ts_username').value,
+        email: document.getElementById('ts_email').value,
+        password: document.getElementById('ts_pwp').value        
+    
+    }   
+    
+    // set content type to application/json
+    
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });    
+        
+    // send the data to the api
+        
+    this.http.post(this.apiURL, JSON.stringify(userData), options)
+        .subscribe(response => {
+            alert(response.json());
+        });
+        
+    return false;
+    
+  }
 
   ngOnInit() {
   }
