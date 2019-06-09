b4d2c0b7d482fbc399e2b92ed41680b27be83185
Mohammed Hamouda <51118790+MoHamouda97@users.noreply.github.com>
2019-06-09 17:08:37 +0300

    Update login.component.ts

--- parashot/src/app/modules/auth/login/login.component.ts
+++ parashot/src/app/modules/auth/login/login.component.ts
@@ -1,3 +1,6 @@
+import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import {Headers, RequestOptions} from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService, FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
 
@@ -8,7 +11,37 @@ import { AuthService, FacebookLoginProvider, GoogleLoginProvider } from "angular
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  private apiURL = 'http://parashote.codesroots.com/api/users/token.json';
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
+  
+  // get user 
+  
+  getUser () {
+  
+    let userData = {
+        
+        username: document.getElementById('ts_username').value,
+        password: document.getElementById('ts_pwp').value        
+    
+    }   
+    
+    
+    // set content type to application/json
+    
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });    
+        
+    // send the data to the api
+        
+    this.http.get(this.apiURL, JSON.stringify(userData), options)
+        .subscribe(response => {
+            alert(response.json());
+        });
+        
+    return false;
+  
+  }
 
   ngOnInit() {
   }
