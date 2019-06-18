7ce34306d5a03ad6919ab26c9ba565a29db55bf2
MoHamouda97 <m97.business@gmail.com>
2019-06-18 16:42:41 +0300

    'categories'

--- parashot/src/app/modules/home/categories/categories.component.ts
+++ parashot/src/app/modules/home/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  FileUploader } from 'ng2-file-upload/ng2-file-upload';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
+  imgSrc;
+
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  // show category image to user when choose an image
+  
+  showImage(event) {
+  
+    if (event.target.files && event.target.files[0]) {
+    
+        let reader = new FileReader();        
+        reader.readAsDataURL(event.target.files[0]);
+        
+        reader.onload = (event) => {
+        
+            this.imgSrc = event.target.result;
+        
+        }
+    
+    }
+  
+  }
+  
+  // remove image that user uploaded
+  
+  resetImg() {
+  
+    this.imgSrc = null;
+  
   }
 
 }
