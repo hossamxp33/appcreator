fb6840e9959c7cfde2f8249a80e90d6ebf7b6bb0
MoHamouda97 <m97.business@gmail.com>
2019-06-20 17:32:50 +0300

    create add product form in product.component

--- parashot/src/app/modules/home/products/products.component.ts
+++ parashot/src/app/modules/home/products/products.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  color = '#000';
+  imgSrc;
+  items: number[] = [1];
+  sizes: number[] = [1];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // display image
+
+  showImage(event) {
+
+    if (event.target.files && event.target.files[0]) {
+
+      let reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event: any) => {
+        this.imgSrc = event.target.result;
+      }
+
+    }
+
+  }
+
+  copyColor() {
+    this.items.push(this.items.length + 1);
+  }
+  
+  copySize(){
+    this.sizes.push(this.sizes.length + 1);
   }
 
 }
